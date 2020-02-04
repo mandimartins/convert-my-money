6456f2ed8f1dace062bcdf11240b2247ca429872
mandimartins <amandamartins23@gmail.com>
2020-02-04 08:09:59 -0300

    Add view engine

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, error => {
   if (error) {
-    console.log('Failed starting the server', error);
+    console.log('Failed to start the server', error);
   } else {
     console.log(`Listening on port ${port}`);
   }
